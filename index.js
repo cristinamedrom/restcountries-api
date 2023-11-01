c8a63a061fa171ae9901f9e22b503ac6c63eeb0e
cristinamedrom <cristinamedranoroman@gmail.com>
2023-11-01 13:01:11 +0100

    listado falta imagenes

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const cardTemplate = function (infoCountries) {
   const { name, flags } = infoCountries;
 
   if (flags) {
-
-    const pngFlag = flags[0]?.png || '';
+    const flagImg = flags[0]?.png || '';
+    return `
+      <div class="card">
+        <img id="flag-image" src="${flagImg}" alt="${name.common}" />
+        <h1 class="center">${name.common}</h1>
+      </div>
+    `;
+  } else {
+    return '';
   }
-
-  return `<div class="card">
-              <img id="flag-image" src=" ${pngFlag} " alt=" ${name.common} " />
-              <h1 class="center">${name.coomon}</h1>
-            </div>`;
 };
 
 const countriesNode = document.getElementById("countries");
@@ -21,18 +23,22 @@ fetch('https://restcountries.com/v3.1/all')
     // To extract the JSON body content from the response, 
     // we use the json() method and pass it into the next .then()
 
-    if (!response.ok) {
-      throw new Error('Error al realizar la solicitud')
+if (!response.ok) {
+      throw new Error('Error al realizar la solicitud');
     }
-    return response.json()
-
+    return response.json();
   })
-
   .then(function (countries) {
     // Here is where you'll need to add into the DOM all the countries received from API 
-
     // 1 - We will need to iterate the countries variable with a loop
     // 2 - You can use the cardTemplate() function to create a div with a class card already styled
     // 💡 you can use countriesNode variable to add elements
-    
+
+    countries.forEach(function (infoCountries) {
+      const detallesCountry = cardTemplate(infoCountries);
+      countriesNode.innerHTML += detallesCountry;
+    });
+  })
+  .catch(function (error) {
+    console.error('Error al obtener los datos');
   });
